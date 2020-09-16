22799787adf6231f6072cd83031ab0504b3916a6
Raihan Islam <raihanislam.cse@gmail.com>
2020-09-16 21:46:02 +0600

    update files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Speece from './Speece';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Speece />
+    </div>
+  );
+}
+
+export default App;
